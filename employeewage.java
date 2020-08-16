db51c5b92827963d7a040ce3333032008c6cae9e
AlokSrivastava <aloksrivastava.0696@gmail.com>
2020-08-16 14:04:36 +0530

    Daily Emp Wage Calculation

--- employeewage.java
+++ employeewage.java
@@ -1,20 +1,30 @@
+import java.util.Arrays;
 import java.util.Random;
 
 public class employeewage {
 
 	public static void main(String[] args) {
+		int EMP_WAGE_PER_HR=20;
+		int EMP_FULL_DAY_HR=8;
+		int NO_OF_WORK_DAY=7;
 		System.out.println("Welcome to Employee Wage Computaion Program.");
 		int check = attendaceCheck();
-		if(check==1) {
-			System.out.println("Employee is present");
-		}
-		else {
-			System.out.println("Employee is absent");
-		}
+		System.out.println("Employee daily wages are:");
+		System.out.println(Arrays.toString(getDailyEmpWage(EMP_WAGE_PER_HR, EMP_FULL_DAY_HR, NO_OF_WORK_DAY)));
 	}
 	
 	public static int attendaceCheck() {
 		Random rand = new Random();
 		return rand.nextInt(2);
+	}
+	
+	public static int[] getDailyEmpWage(int wagehr, int dayhr, int workday) {
+		int totalWorkDay=0;
+		int dailyWage[] = new int [workday];
+		while(totalWorkDay<workday) {
+			int attendance = attendaceCheck();
+			dailyWage [totalWorkDay++] = (attendance*dayhr*wagehr);
+		}
+		return dailyWage;
 	}
 }
