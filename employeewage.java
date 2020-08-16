fad6b8e86ec046668d1d8dbd666fe544fc6c6c60
AlokSrivastava <aloksrivastava.0696@gmail.com>
2020-08-16 14:42:54 +0530

    Adding Part Time And Wage

--- employeewage.java
+++ employeewage.java
@@ -6,11 +6,32 @@ public class employeewage {
 	public static void main(String[] args) {
 		int EMP_WAGE_PER_HR=20;
 		int EMP_FULL_DAY_HR=8;
+		int EMP_PART_DAY_HR=4;
 		int NO_OF_WORK_DAY=7;
+		int totalEmpHr=0;
+		int totalWorkDay=0;
+		int isFullTime=0;
+		int EMP_DAY_HR=0;
 		System.out.println("Welcome to Employee Wage Computaion Program.");
-		int check = attendaceCheck();
-		System.out.println("Employee daily wages are:");
-		System.out.println(Arrays.toString(getDailyEmpWage(EMP_WAGE_PER_HR, EMP_FULL_DAY_HR, NO_OF_WORK_DAY)));
+		int check = 0;
+		int totalWage=0;
+		while (totalWorkDay<NO_OF_WORK_DAY) {
+			Random r = new Random();
+			isFullTime=r.nextInt(2);
+			if (isFullTime==1) {
+				EMP_DAY_HR=EMP_FULL_DAY_HR;
+			}
+			else {
+				EMP_DAY_HR=EMP_PART_DAY_HR;
+			}
+			check=attendaceCheck();
+			totalEmpHr+=(check*EMP_DAY_HR);
+			System.out.println("Employee wage for Day"+(totalWorkDay+1)+" is ");
+			System.out.println(getDailyEmpWage(totalWorkDay, check, EMP_DAY_HR, EMP_WAGE_PER_HR));
+			totalWorkDay++;
+		}
+		totalWage=(totalEmpHr*EMP_WAGE_PER_HR);
+		System.out.println("Total wage of the employee is:"+totalWage);
 	}
 	
 	public static int attendaceCheck() {
@@ -18,13 +39,7 @@ public class employeewage {
 		return rand.nextInt(2);
 	}
 	
-	public static int[] getDailyEmpWage(int wagehr, int dayhr, int workday) {
-		int totalWorkDay=0;
-		int dailyWage[] = new int [workday];
-		while(totalWorkDay<workday) {
-			int attendance = attendaceCheck();
-			dailyWage [totalWorkDay++] = (attendance*dayhr*wagehr);
-		}
-		return dailyWage;
+	public static int getDailyEmpWage(int tworkday, int attendace, int dayhr, int wagehr) {
+		 return (attendace*dayhr*wagehr);
 	}
 }
